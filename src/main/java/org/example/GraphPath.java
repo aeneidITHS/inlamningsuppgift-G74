f4cce6b51990b8f9ebbd771d1f35a615c5754c00
AndyMcPandy <74055809+AndyMcPandy@users.noreply.github.com>
2026-05-11 15:24:18 +0200

    Add files via upload

---
+++ src/main/java/org/example/GraphPath.java
@@ -0,0 +1,74 @@
+package org.example;
+
+import java.util.*;
+
+public class GraphPath <T> implements Path<T> {
+    private final T start;
+    private final List<Edge<T>> edges;
+
+    public GraphPath(T start, List<Edge<T>> edges){
+        this.start = start;
+        this.edges = new ArrayList<>(edges);
+
+    }
+
+    @Override
+    public Iterator<Edge<T>> iterator() {
+        return edges.iterator();
+    }
+
+    @Override
+    public T getStart() {
+        return start;
+    }
+
+    @Override
+    public T getEnd() {
+        if (edges.isEmpty()) {
+            return start;
+        }
+        return edges.get(edges.size() - 1).getDestination();
+    }
+
+    @Override
+    public int getTotalWeight() {
+        int total = 0;
+        for (Edge<T> edge : edges) {
+            total += edge.getWeight();
+        }
+        return total;
+    }
+
+    @Override
+    public List<Edge<T>> getEdges() {
+        return Collections.unmodifiableList(edges);
+    }
+
+    @Override
+    public List<T> getNodes() {
+        List<T> nodes = new ArrayList<>();
+        nodes.add(start);
+        for (Edge<T> edge : edges) {
+            nodes.add(edge.getDestination());
+        }
+        return nodes;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Start: ").append(start);
+        for(Edge<T> edge: edges)  {
+            sb.append(edge).append("\n");
+        }
+        sb.append("End: ").append(getEnd()).append("\n");
+        sb.append("Totalweight: ").append(getTotalWeight());
+        return sb.toString();
+
+    }
+
+
+
+
+}
