13214630f9770d8c451a04ceec20730d1e2588db
McFishyTheThird <william.lignell@gmail.com>
2026-05-25 18:29:47 +0200

    RouteEdgeView

--- src/main/java/org/example/frontend/RouteEdgeView.java
+++ src/main/java/org/example/frontend/RouteEdgeView.java
@@ -1,4 +1,17 @@
 package org.example.frontend;
+import org.example.backend.*;
 
-public class RouteEdgeView {
+import javafx.scene.Group;
+import javafx.scene.shape.Line;
+
+public class RouteEdgeView extends Group{
+    public RouteEdgeView(Path<City> path) {
+        for (int i = 0; i < path.getEdges().size(); i++) {
+            Edge<City> edge = path.getEdges().get(i);
+            City from = edge.getFrom();
+            City to = edge.getDestination();
+            Line line = new Line(from.getX(), from.getY(), to.getX(), to.getY());
+            getChildren().add(line);
+        }
+    }
 }
