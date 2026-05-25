79663b090bea42600924bead12835cf887c50c71
AndyMcPandy <74055809+AndyMcPandy@users.noreply.github.com>
2026-05-25 17:07:46 +0200

    Update CityNodeView.java

--- src/main/java/org/example/frontend/CityNodeView.java
+++ src/main/java/org/example/frontend/CityNodeView.java
@@ -1,28 +1,62 @@
 package org.example.frontend;
 
+
+import javafx.event.EventHandler;
 import javafx.scene.Group;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
-import java.awt.*;
-
 public class CityNodeView extends Group {
 
-    private City city;
-    private Circle circle;
+   private City city;
+   private Circle circle;
+   private double startX, startY;
 
-    public CityNodeView(City city){
-        this.city = city;
 
-        this.circle = new Circle(city.getX(),city.getY(), 12);
-        circle.setFill(Color.ALICEBLUE);
-        circle.setStroke(Color.BLACK);
-        Label label = new Label(city.getName());
-        label.setLocation(city.getX(),city.getY());
-        getChildren().add(circle);
-    }
+   public CityNodeView(City city) {
+       this.city = city;
 
-    public Circle getCircle() {
-        return circle;
-    }
+
+       relocate(city.getX(), city.getY());
+
+
+       circle = new Circle(0, 0, 12);
+       circle.setFill(Color.ALICEBLUE);
+       circle.setStroke(Color.BLACK);
+
+
+       Label label = new Label(city.getName());
+       label.setLayoutX(-10);
+       label.setLayoutY(15);
+
+
+       getChildren().addAll(circle, label);
+
+
+       setOnMousePressed(new StartDragHandler());
+       setOnMouseDragged(new DragHandler());
+   }
+
+
+   class StartDragHandler implements EventHandler<MouseEvent> {
+       public void handle(MouseEvent event) {
+           startX = event.getX();
+           startY = event.getY();
+       }
+   }
+
+
+   class DragHandler implements EventHandler<MouseEvent> {
+       public void handle(MouseEvent event) {
+           double newX = getLayoutX() + event.getX() - startX;
+           double newY = getLayoutY() + event.getY() - startY;
+           relocate(newX, newY);
+       }
+   }
+
+
+   public Circle getCircle() { return circle; }
+   public City getCity() { return city; }
 }
