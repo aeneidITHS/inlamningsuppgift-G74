12d207622e9855e382e449dfba38e27c1495f2f8
McFishyTheThird <william.lignell@gmail.com>
2026-05-26 17:52:28 +0200

    fixad routeview

--- src/main/java/org/example/frontend/RouteEdgeView.java
+++ src/main/java/org/example/frontend/RouteEdgeView.java
@@ -8,7 +8,7 @@ public class RouteEdgeView extends Group{
     public RouteEdgeView(Path<City> path) {
         for (int i = 0; i < path.getEdges().size(); i++) {
             Edge<City> edge = path.getEdges().get(i);
-            City from = edge.getFrom();
+            City from = (i == 0) ? path.getStart() : path.getEdges().get(i - 1).getDestination();
             City to = edge.getDestination();
             Line line = new Line(from.getX(), from.getY(), to.getX(), to.getY());
             getChildren().add(line);
