b038722a8df70abc128929869a70f1af1fca24c1
McFishyTheThird <william.lignell@gmail.com>
2026-05-27 17:21:36 +0200

    fixade routeedgeview

--- src/main/java/org/example/frontend/RouteEdgeView.java
+++ src/main/java/org/example/frontend/RouteEdgeView.java
@@ -1,17 +1,15 @@
 package org.example.frontend;
 
-import org.example.backend.*;
-
 import javafx.scene.Group;
 import javafx.scene.shape.Line;
+import java.util.ArrayList;
 
 public class RouteEdgeView extends Group{
-    public RouteEdgeView(Path<City> path) {
-        for (int i = 0; i < path.getEdges().size(); i++) {
-            Edge<City> edge = path.getEdges().get(i);
-            City from = (i == 0) ? path.getStart() : path.getEdges().get(i - 1).getDestination();
-            City to = edge.getDestination();
-            Line line = new Line(from.x(), from.y(), to.x(), to.y());
+
+    public RouteEdgeView(ArrayList<double[]> coordinates) {
+
+        for (int i = 0; i < coordinates.size(); i++) {
+            Line line = new Line(coordinates.get(i)[0], coordinates.get(i)[1], coordinates.get(i)[2], coordinates.get(i)[3]);
             getChildren().add(line);
         }
     }
