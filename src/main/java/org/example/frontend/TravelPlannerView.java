a1cbd38298cd3cedb7e4ce8019911d7b8e3c9401
AndyMcPandy <74055809+AndyMcPandy@users.noreply.github.com>
2026-05-28 02:13:02 +0200

    Update TravelPlannerView.java

--- src/main/java/org/example/frontend/TravelPlannerView.java
+++ src/main/java/org/example/frontend/TravelPlannerView.java
@@ -15,6 +15,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import javafx.geometry.Pos
 
 
 public class TravelPlannerView extends BorderPane {
